ade59fd1bd695bcf9cf20179f31eb5194db67fb9
Barnabas James <barnabasjames27@gmail.com>
2025-02-07 22:31:37 +0100

    Lolll js is fun

--- home.js
+++ home.js
@@ -1,17 +1,46 @@
 const header = document.getElementById("hero");
 const navButtons = document.querySelectorAll(".nav-button");
 
-const navDrops = document.getElementById("vehicle")
+const navDrops = document.querySelectorAll(".nav-drop")
+let isHovering = false
 
 navButtons.forEach(button => {
     button.addEventListener('mouseover', () => {
         header.toggleAttribute("hovered")
-        navDrops.toggleAttribute("hovered")
+
+        navDrops.forEach(div=> {
+            div.style.display = "flex";
+            
+            // div.addEventListener('mouseenter', () => {
+            //     isHovering = true;
+            // });
+
+            // div.addEventListener('mouseleave', () => {
+            //     isHovering = false;
+            // });
+
+        })
     })
 
     button.addEventListener('mouseout', () =>{
-        header.toggleAttribute("hovered")  
-        navDrops.toggleAttribute("hovered")  
+        // header.toggleAttribute("hovered")  
+
+        navDrops.forEach(div=> {
+
+            div.addEventListener('mouseenter', () => {
+                isHovering = true;
+            });
+
+            div.addEventListener('mouseleave', () => {
+                isHovering = false;
+            });
+
+            if(!isHovering) {
+                div.style.display = "flex";
+            } else {
+                div.style.display = "none";
+            }
+        })
     })
 })
 
